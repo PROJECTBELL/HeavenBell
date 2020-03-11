fffdc729446b253d248dc6c0b762002aefeca956
unknown <n.lefebvre@ecole-ipssi.net>
2020-03-11 16:45:29 +0100

    init commit

--- Readme.md
+++ Readme.md
@@ -1,1 +1,14 @@
-test md
+## URL
+```
+127.0.0.1:3000
+```
+
+## Launch docker
+```
+docker-compose up
+```
+
+## Access to docker
+```
+docker exec -ti container bash
+```
---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const server = express();
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinodeipssi');
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+const postRoute = require('./api/routes/postRoute');
+postRoute(server);
+
+server.listen(port, hostname);
